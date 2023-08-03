f2c16bff9a8e664b53bd476e31e2a1052c441493
lucas-hirata <lucashirata.dev@gmail.com>
2023-08-03 20:00:48 -0300

    Created db context, create hotel and country, seeded data

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data;
+
+public class HotelListingDbContext : DbContext
+{
+    public HotelListingDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasData(
+            new
+            {
+                Id = 1,
+                Name = "Brazil",
+                ShortName = "BR",
+            },
+            new
+            {
+                Id = 2,
+                Name = "Bahamas",
+                ShortName = "BS",
+            },
+            new
+            {
+                Id = 3,
+                Name = "Italy",
+                ShortName = "IT",
+            }
+        );
+        modelBuilder.Entity<Hotel>().HasData(
+            new
+            {
+                Id = 1,
+                Name = "Hotel BR",
+                Address = "Rua talz, bairro",
+                CountryId = 1,
+                Rating = 2.5
+            },
+            new
+            {
+                Id = 2,
+                Name = "Hotel BS",
+                Address = "Uau que lugar, lindo",
+                CountryId = 2,
+                Rating = 3.5
+            },
+            new
+            {
+                Id = 3,
+                Name = "Hotel Pizza",
+                Address = "Rua pizza, pizza",
+                CountryId = 3,
+                Rating = 5.0
+            }
+        );
+    }
+}
---
+++ HotelListing.API/Migrations/20230803225910_SeededCountriesAndHotels.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Brazil", "BR" },
+                    { 2, "Bahamas", "BS" },
+                    { 3, "Italy", "IT" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Rua talz, bairro", 1, "Hotel BR", 2.5 },
+                    { 2, "Uau que lugar, lindo", 2, "Hotel BS", 3.5 },
+                    { 3, "Rua pizza, pizza", 3, "Hotel Pizza", 5.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
