9e9726e318f5236d65b12b6091c55e5b01ce10fe
lucas-hirata <lucashirata.dev@gmail.com>
2023-08-24 17:37:44 -0300

    Configured Identity Core

---
+++ HotelListing.API/Data/Configuration/RoleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Data.Configuration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
